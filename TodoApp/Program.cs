07b408fa6965faa34841f941241a50fb726d19fa
manLuke <kachatomas@gmail.com>
2022-09-20 13:52:13 +0200

    add simple TodoApp

---
+++ TodoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+            var todos = new List<String>();
+            Console.WriteLine("Type showMyTodos if you want to see all your Todos");
+            Console.WriteLine("Type exit if you want to exit");
+            Console.WriteLine("Just type your Todo, which you want to add");
+            while (!end)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Add todo: ");
+                string todo = Console.ReadLine();
+                if (todo == "showMyTodos") {
+                    Console.WriteLine("");
+                    Console.WriteLine("Your Todos: ");
+                    todos.ForEach(Console.WriteLine);
+                    Console.WriteLine("");
+                } else if (todo == "exit") {
+                    end = true;
+                } else {
+                    todos.Add(todo);
+                }
+            }
+        }
+    }
+}
