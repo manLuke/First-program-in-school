dc03db8e802e09fdee9997b5ca22e93620e01920
tomas_kacha <86781614+manLuke@users.noreply.github.com>
2022-09-20 09:25:30 +0200

    init

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1; 
+            while (n <= 200)
+            {
+                Console.WriteLine($"Hello world, for {n} times");
+                n++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
